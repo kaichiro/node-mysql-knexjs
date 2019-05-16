5ab6fdeecc44e1950958b977f2dfa9cfeae713eb
kaichiro <kcfg23@gmail.com>
2019-05-16 14:37:51 -0300

    Refatorando legibilidade do código

--- controllers/pessoas.js
+++ controllers/pessoas.js
@@ -5,25 +5,25 @@ const index = async (db, req, res) => {
     res.render('pessoas/index', { pessoas: results })
 }
 
-const deleteOne = async (connection, req, res) => {
-    await pessoas.deleteOne(connection, req.params.id)
+const deleteOne = async (db, req, res) => {
+    await pessoas.deleteOne(db, req.params.id)
     res.redirect('/pessoas')
 }
 
 const createForm = (req, res) => {
     res.render('pessoas/create')
 }
-const createProcess = async (connection, req, res) => {
-    await pessoas.create(connection, req.body)
+const createProcess = async (db, req, res) => {
+    await pessoas.create(db, req.body)
     res.redirect('/pessoas')
 }
 
-const updateForm = async (connection, req, res) => {
-    const pessoa = await pessoas.findById(connection, req.params.id)
+const updateForm = async (db, req, res) => {
+    const pessoa = await pessoas.findById(db, req.params.id)
     res.render('pessoas/update', { pessoa })
 }
-const updateProcess = async (connection, req, res) => {
-    await pessoas.update(connection, req.params.id, req.body)
+const updateProcess = async (db, req, res) => {
+    await pessoas.update(db, req.params.id, req.body)
     res.redirect('/pessoas')
 }
 
--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const app = express()
 const bodyParser = require('body-parser')
 
 require('dotenv/config')
-const port = process.env.APP_PORT || 3000
 
 const db = require('knex')({
     client: 'mysql2',
@@ -14,25 +13,26 @@ const db = require('knex')({
         user: 'root',
         password: '',
         database: 'cadastro',
-    }
+    },
 })
 
 const dependecies = {
     db,
 }
 
-const pessoas = require('./routes/pessoas')
-
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.render('home'))
-app.use('/pessoas', pessoas(dependecies))
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 
+const pessoas = require('./routes/pessoas')
+app.use('/pessoas', pessoas(dependecies))
+
 const address = 'http://localhost'
+const port = process.env.APP_PORT || 3000
 
 app.listen(port, () =>
     console.log('\nDatabase is connect.\n'.concat(`App listening on ${address}:${port}`))
